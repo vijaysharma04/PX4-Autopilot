484e60d117eefccbee2ffbdbbe4518c74bb08f21
vijaysharma04 <vijaysharma04@users.noreply.github.com>
2026-08-07 16:50:40 +0530

    Require dual-stick inward CSC for arming

--- src/modules/manual_control/ManualControl.cpp
+++ src/modules/manual_control/ManualControl.cpp
@@ -364,9 +364,10 @@ void ManualControl::processStickArming(const manual_control_setpoint_s &input)
 	// Arm gesture
 	const bool right_stick_centered = (fabsf(input.pitch) < 0.1f) && (fabsf(input.roll) < 0.1f);
 	const bool left_stick_lower_right = (input.throttle < -0.8f) && (input.yaw > 0.9f);
+	const bool right_stick_lower_left = (input.pitch < -0.9f) && (input.roll < -0.9f);
 
 	const bool previous_stick_arm_hysteresis = _stick_arm_hysteresis.get_state();
-	_stick_arm_hysteresis.set_state_and_update(left_stick_lower_right && right_stick_centered, input.timestamp);
+	_stick_arm_hysteresis.set_state_and_update(left_stick_lower_right && right_stick_lower_left, input.timestamp);
 
 	if (_param_man_arm_gesture.get() && !previous_stick_arm_hysteresis && _stick_arm_hysteresis.get_state()) {
 		sendActionRequest(action_request_s::ACTION_ARM, action_request_s::SOURCE_RC_STICK_GESTURE);
